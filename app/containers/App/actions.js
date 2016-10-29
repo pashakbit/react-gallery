300640bcc25e6feb4918c893833233f98dfcfa94
Kokovin Pavel <kokovinpavel@yandex.ru>
2016-10-29 19:46:46 +0700

    Restructured logic for work with users

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -1,63 +1,25 @@
-/*
- * App Actions
- *
- * Actions change things in your application
- * Since this boilerplate uses a uni-directional data flow, specifically redux,
- * we have these actions which are the only way your application interacts with
- * your application state. This guarantees that your state is up to date and nobody
- * messes it up weirdly somewhere.
- *
- * To add a new Action:
- * 1) Import your constant
- * 2) Add a function like this:
- *    export function yourAction(var) {
- *        return { type: YOUR_ACTION_CONSTANT, var: var }
- *    }
- */
-
 import {
-  LOAD_REPOS,
-  LOAD_REPOS_SUCCESS,
-  LOAD_REPOS_ERROR,
+  LOAD_USER,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_ERROR,
 } from './constants';
 
-/**
- * Load the repositories, this action starts the request saga
- *
- * @return {object} An action object with a type of LOAD_REPOS
- */
-export function loadRepos() {
+export function loadUser() {
   return {
-    type: LOAD_REPOS,
+    type: LOAD_USER,
   };
 }
 
-/**
- * Dispatched when the repositories are loaded by the request saga
- *
- * @param  {array} repos The repository data
- * @param  {string} username The current username
- *
- * @return {object}      An action object with a type of LOAD_REPOS_SUCCESS passing the repos
- */
-export function reposLoaded(repos, username) {
+export function loadUserSuccess(user) {
   return {
-    type: LOAD_REPOS_SUCCESS,
-    repos,
-    username,
+    type: LOAD_USER_SUCCESS,
+    user,
   };
 }
 
-/**
- * Dispatched when loading the repositories fails
- *
- * @param  {object} error The error
- *
- * @return {object}       An action object with a type of LOAD_REPOS_ERROR passing the error
- */
-export function repoLoadingError(error) {
+export function loadUserError(error) {
   return {
-    type: LOAD_REPOS_ERROR,
+    type: LOAD_USER_ERROR,
     error,
   };
 }
--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -1,19 +1,7 @@
-/*
- * AppReducer
- *
- * The reducer takes care of our data. Using actions, we can change our
- * application state.
- * To add a new action, add it to the switch statement in the reducer function
- *
- * Example:
- * case YOUR_ACTION_CONSTANT:
- *   return state.set('yourStateVariable', true);
- */
-
 import {
-  LOAD_REPOS_SUCCESS,
-  LOAD_REPOS,
-  LOAD_REPOS_ERROR,
+  LOAD_GALLERY,
+  LOAD_GALLERY_SUCCESS,
+  LOAD_GALLERY_ERROR,
 } from './constants';
 import { fromJS } from 'immutable';
 
@@ -21,25 +9,22 @@ import { fromJS } from 'immutable';
 const initialState = fromJS({
   loading: false,
   error: false,
-  currentUser: false,
-  userData: fromJS({
-    repositories: false,
+  data: fromJS({
+    user: {},
   }),
 });
 
 function appReducer(state = initialState, action) {
   switch (action.type) {
-    case LOAD_REPOS:
+    case LOAD_GALLERY:
       return state
         .set('loading', true)
-        .set('error', false)
-        .setIn(['userData', 'repositories'], false);
-    case LOAD_REPOS_SUCCESS:
+        .set('error', false);
+    case LOAD_GALLERY_SUCCESS:
       return state
-        .setIn(['userData', 'repositories'], action.repos)
-        .set('loading', false)
-        .set('currentUser', action.username);
-    case LOAD_REPOS_ERROR:
+        .setIn(['data', 'user'], action.user)
+        .set('loading', false);
+    case LOAD_GALLERY_ERROR:
       return state
         .set('error', action.error)
         .set('loading', false);
