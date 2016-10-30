c290c464686e96ec9632059f6a1b876435a5bc5a
Kokovin Pavel <kokovinpavel@yandex.ru>
2016-10-30 23:08:37 +0700

    Changed constants in App reduser

--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -1,7 +1,7 @@
 import {
-  LOAD_GALLERY,
-  LOAD_GALLERY_SUCCESS,
-  LOAD_GALLERY_ERROR,
+  LOAD_USER,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_ERROR,
 } from './constants';
 import { fromJS } from 'immutable';
 
@@ -16,15 +16,15 @@ const initialState = fromJS({
 
 function appReducer(state = initialState, action) {
   switch (action.type) {
-    case LOAD_GALLERY:
+    case LOAD_USER:
       return state
         .set('loading', true)
         .set('error', false);
-    case LOAD_GALLERY_SUCCESS:
+    case LOAD_USER_SUCCESS:
       return state
         .setIn(['data', 'user'], action.user)
         .set('loading', false);
-    case LOAD_GALLERY_ERROR:
+    case LOAD_USER_ERROR:
       return state
         .set('error', action.error)
         .set('loading', false);
